02b838d5e5f7c7d085152aec3ac7e6d09ff108a1
Jeremy Groce <JJGroce95@gmail.com>
2024-10-13 23:37:57 -0500

    beginning work on task tracker

--- src/pages/taskTracker.js
+++ src/pages/taskTracker.js
@@ -1,17 +1,60 @@
 import React from "react";
 import DashNav from "../components/dashboard/dashNav";
 
+import '../styling/taskTracker.css';
+
 function TaskTracker()
 {
     return(
         <div>
             <DashNav/>
             
-            <div className="tasktracker-component">
-                
+
+            <div className="taskTracker-page">
+
+                <div className="taskTracker-outer-container">
+                    
+                    {/* day of the week display */}
+                    <div className="taskTracker-week-container">
+
+                        {/* Day modules */}
+                        <div className="taskTracker-week-day-container">
+
+                        </div>
+
+                        <div className="taskTracker-week-day-container">
+
+                        </div>
+
+                        <div className="taskTracker-week-day-container">
+
+                        </div>
+
+                        <div className="taskTracker-week-day-container">
+
+                        </div>
+
+                        <div className="taskTracker-week-day-container">
+
+                        </div>
+
+                        <div className="taskTracker-week-day-container">
+
+                        </div>
+
+                        <div className="taskTracker-week-day-container">
+
+                        </div>
+                        
+
+                    </div>
+
+                    {/* Daily-Weekly-Monthly Dask display */}
 
 
-            </div>
+
+                    </div>
+                </div>
 
         </div>
     );
